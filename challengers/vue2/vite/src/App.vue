d6e2d754de5d5a165f585cd32844cc055697d61c
Ivan Sitkin <alviner@yandex-team.ru>
2023-03-31 11:45:56 +0300

    feat: vue challengers

---
+++ challengers/vue2/vite/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: {HelloWorld},
+}
+</script>
+
+<template>
+  <div>
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://v2.vuejs.org" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
+  </div>
+</template>
---
+++ challengers/vue2/vite/src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import './style.css'
+import App from './App.vue'
+
+export default new Vue({
+  el: '#app',
+  render: (h) => h(App)
+})
