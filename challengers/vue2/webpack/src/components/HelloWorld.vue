5fd4205f59c0a1f1d0fffc5d1d96734ce919bba6
Ivan Sitkin <alvinera@yandex.ru>
2023-04-19 14:49:48 +0300

    feat: webpack

---
+++ challengers/vue2/webpack/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<script>
+
+export default {
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      count: 0
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+
+    <div class="card">
+      <button type="button" @click="count++">count is {{ count }}</button>
+    </div>
+
+    <p class="read-the-docs">Click on Webpack and Vue logos to learn more</p>
+
+  </div>
+</template>
